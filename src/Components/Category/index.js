5eb7df828c445167584756928bb8d536539ec30b
XfirePc <vinygfx@gmail.com>
2019-09-11 01:00:11 -0500

    apply-styled-components

--- src/Components/Category/index.js
+++ src/Components/Category/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Anchor, Image } from './styles'
 
 const DEFAULT_IMAGE = 'http://icons.iconarchive.com/icons/sonya/swarm/256/Cat-icon.png'
 
 export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = '?' }) => (
-  <a href={path}>
-    <img src={cover} />
+  <Anchor href={path}>
+    <Image src={cover} />
     {emoji}
-  </a>
+  </Anchor>
 )
