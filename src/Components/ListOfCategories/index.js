1607d9d90f305b6fcc12f1c5dadefb80e04db54c
XfirePc <vinygfx@gmail.com>
2019-09-13 01:50:16 -0500

    create fecth with hooks

--- src/Components/ListOfCategories/index.js
+++ src/Components/ListOfCategories/index.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './style'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
-  return (
+  const [categories, setCategories] = useState([])
+  useEffect(function () {
+    window.fetch('https://petgram-server-xfire.vinygfx.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
+  const renderList = () => {
     <List>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  }
+  return (
+    renderList()
   )
 }
