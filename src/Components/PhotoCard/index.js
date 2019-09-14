b048035e932a8bf5d701315b262eb79f627ec0b6
XfirePc <vinygfx@gmail.com>
2019-09-14 00:18:32 -0500

    create custom hooks near screen lazy-loading

--- src/Components/PhotoCard/index.js
+++ src/Components/PhotoCard/index.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useRef, useState, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { ImgWrapper, Img, Button, Article } from './styles.js'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { useLocalStorage } from '../../Hooks/useLocalStorage'
+import { useNearScreen } from '../../Hooks/useNearScreen'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const element = useRef(null)
-  const [show, setShow] = useState(false)
+  const [show, element] = useNearScreen()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
   const Icon = liked ? MdFavorite : MdFavoriteBorder
