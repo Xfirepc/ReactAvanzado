25e657c559a94ecfc0b6814e7d427160b2e651e7
XfirePc <vinygfx@gmail.com>
2019-09-21 00:33:31 -0500

    add mutation graphql with react-apollo to refresh realtime mutation data

--- src/Components/PhotoCard/index.js
+++ src/Components/PhotoCard/index.js
@@ -1,16 +1,15 @@
 import React, { Fragment } from 'react'
-import { ImgWrapper, Img, Button, Article } from './styles.js'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { ImgWrapper, Img, Article } from './styles.js'
 import { useLocalStorage } from '../../Hooks/useLocalStorage'
 import { useNearScreen } from '../../Hooks/useNearScreen'
-
+import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../Container/ToggleLikeMutation'
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={element}>
@@ -21,9 +20,19 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} />
             </ImgWrapper>
           </a>
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size='32px' /> { likes } likes!
-          </Button>
+          <ToggleLikeMutation>
+            {
+              (toggleLike) => {
+                const handleFavClick = () => {
+                  !liked && toggleLike({ variables: {
+                    input: { id }
+                  } })
+                  setLiked(!liked)
+                }
+                return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
+              }
+            }
+          </ToggleLikeMutation>
         </Fragment>
       }
     </Article>
