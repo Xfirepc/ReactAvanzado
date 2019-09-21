ca780117c7f1f4024d09133fb4a65c465c4212fe
XfirePc <vinygfx@gmail.com>
2019-09-20 23:38:18 -0500

    component using render props to do fetching data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { ListOfCategories } from './Components/ListOfCategories'
 import { GlobalStyle } from './Styles/GlogbalStyles'
 import { ListOfPhotoCards } from './Container/ListOfPhotoCards'
+import { PhotoCardWithQuery } from './Container/PhotoCardWithQuery'
 import { Logo } from './Components/Logo'
 
-export const App = () => (
-  <>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={1} />
-  </>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+  console.log(detailId)
+  return (
+    <>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <Fragment>
+            <ListOfCategories />
+            <ListOfPhotoCards categoryId={1} />
+          </Fragment>
+      }
+    </>
+  )
+}
