635fee11784b39146a81737baf434949789c78ab
XfirePc <vinygfx@gmail.com>
2019-10-03 00:12:37 -0500

    IMPORTANTE: PREPARE && TEST PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,47 @@
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+const path = require('path')
+
 module.exports = {
   output: {
     filename: 'app.bundle.js',
     publicPath: '/'
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: 'src/index.html' })
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new WebpackPwaManifest({
+      name: 'Pergram - Las mejores mascotas <3!',
+      short_name: 'Petgram',
+      description: 'Las fotos mas hermosas de tus mascotas',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/Assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-xfire.vinygfx.now.sh/'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
+    })
   ],
   module: {
     rules: [
