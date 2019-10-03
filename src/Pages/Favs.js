234a10e11fe7ac4d8061f196a01f59d03089e3d5
XfirePc <vinygfx@gmail.com>
2019-10-02 21:52:31 -0500

    use react helmet to change seo pages

--- src/Pages/Favs.js
+++ src/Pages/Favs.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { FavsWithQuery } from '../Container/GetFavorites'
+import { Helmet } from 'react-helmet'
 
 export const Favs = () => (
   <>
+    <Helmet>
+      <title>Favoritos - Las mascotas que te han enamorado!</title>\
+      <meta name='description' content='Las mascotas mas hermosas para ti!' />
+    </Helmet>
     <h1> Favoritos </h1>
     <FavsWithQuery />
   </>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react'
 import { ListOfCategories } from '../Components/ListOfCategories'
 import { ListOfPhotoCards } from '../Container/ListOfPhotoCards'
-
+import { Helmet } from 'react-helmet'
 export const Home = ({ id }) => {
   return (
     <Fragment>
+      <Helmet>
+        <title>Petgram - Las mascotas mas bellas!</title>\
+        <meta name='description' content='Comparte las mejores fotos de tus mascotas' />
+      </Helmet>
       <ListOfCategories />
       <ListOfPhotoCards categoryId={id} />
     </Fragment>
