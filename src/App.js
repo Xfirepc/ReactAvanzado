f1092ad27ed69e3987d4086bc38fa57e43f07189
XfirePc <vinygfx@gmail.com>
2019-10-02 21:26:54 -0500

    fix routes with reach router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { GlobalStyle } from './Styles/GlogbalStyles'
 import { Logo } from './Components/Logo'
-
+import { NotFound } from './Pages/NotFound'
 import { Home } from './Pages/Home'
 import { Detail } from './Pages/Detail'
 import { User } from './Pages/User'
@@ -9,33 +9,28 @@ import { Favs } from './Pages/Favs'
 import { NotRegisterUser } from './Pages/NotRegisterUser'
 import { NavBar } from './Components/NavBar'
 
-import { Router } from '@reach/router'
-import Context from './Context'
+import { Router, Redirect } from '@reach/router'
+import { Context } from './Context'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
+  console.log(isAuth)
   return (
     <>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisterUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='/login' noThrow />}
+        {!isAuth && <Redirect from='/user' to='/login' noThrow />}
+        {isAuth && <Redirect from='/login' to='/' noThrow />}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-      <Context.Consumer>
-        {
-          ({ isAuth }) =>
-            isAuth
-              ? <Router>
-                <Favs path='/favs' />
-                <User path='/user' />
-              </Router>
-              : <Router>
-                <NotRegisterUser path='/favs' />
-                <NotRegisterUser path='/user' />
-              </Router>
-        }
-      </Context.Consumer>
       <NavBar />
     </>
   )
