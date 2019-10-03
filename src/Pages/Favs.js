8eb1a9a6634082e43413cb0205f7c02f91604b76
XfirePc <vinygfx@gmail.com>
2019-10-02 22:23:17 -0500

    create componenet to use react-helmet

--- src/Pages/Favs.js
+++ src/Pages/Favs.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import { FavsWithQuery } from '../Container/GetFavorites'
-import { Helmet } from 'react-helmet'
+import { Layout } from '../Components/Layout'
 
 export const Favs = () => (
   <>
-    <Helmet>
-      <title>Favoritos - Las mascotas que te han enamorado!</title>\
-      <meta name='description' content='Las mascotas mas hermosas para ti!' />
-    </Helmet>
-    <h1> Favoritos </h1>
+    <Layout title='Favoritos - Las mascotas que te han enamorado!' subtitle='Las mascotas mas hermosas para ti!' />
     <FavsWithQuery />
   </>
 )
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,12 @@
 import React, { Fragment } from 'react'
 import { ListOfCategories } from '../Components/ListOfCategories'
 import { ListOfPhotoCards } from '../Container/ListOfPhotoCards'
-import { Helmet } from 'react-helmet'
+import { Layout } from '../Components/Layout'
+
 export const Home = ({ id }) => {
   return (
     <Fragment>
-      <Helmet>
-        <title>Petgram - Las mascotas mas bellas!</title>\
-        <meta name='description' content='Comparte las mejores fotos de tus mascotas' />
-      </Helmet>
+      <Layout title='Petgram - Las mascotas mas bellas!' subtitle='Comparte las mejores fotos de tus mascotas' />
       <ListOfCategories />
       <ListOfPhotoCards categoryId={id} />
     </Fragment>
